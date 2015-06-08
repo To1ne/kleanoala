66f5eda0544b479acabfa47520e1de3fb1120a8d
Toon Claes <toon@iotcl.com>
2015-06-08 23:10:09 +0200

    Make scripts work with local and remote server

--- now.sh
+++ now.sh
@@ -1,1 +1,8 @@
-curl -H "Content-Type: application/json" http://localhost:4567/now
+if -z "$PROD"
+then
+  URL="http://localhost:4567/put"
+else
+  URL="https://kleanoala.herokuapp.com/cleaners"
+fi
+
+curl -H "Content-Type: application/json" $URL
--- put.sh
+++ put.sh
@@ -1,1 +1,8 @@
-curl -H "Content-Type: application/json" -X PUT -d '{"startdate":"2015/06/01","cleaners":["nate", "toon", "reprazent", "pjaspers", "soffe", "gregory", "bram", "lewis", "atog", "buwaro", "tomklaasen"]}' http://localhost:4567/cleaners -u admin:$ADMIN_PASSWORD
+if -z "$PROD"
+then
+  URL="http://localhost:4567/put"
+else
+  URL="https://kleanoala.herokuapp.com/cleaners"
+fi
+
+curl -H "Content-Type: application/json" -X PUT -d '{"startdate":"2015/06/01","cleaners":["nate", "toon", "reprazent", "pjaspers", "soffe", "gregory", "bram", "lewis", "atog", "buwaro", "tomklaasen"]}' $URL -u admin:$ADMIN_PASSWORD
